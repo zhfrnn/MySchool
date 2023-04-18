9e2e60f011b93f445d7c759c0ca2da979a77eee7
zhfrnn <zhafran.ph@gmail.com>
2023-04-18 21:22:56 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// var counter = 1;
+// setInterval(function () {
+//   document.getElementById("radio" + counter).checked = true;
+//   counter++;
+//   if (counter > 3) {
+//     counter = 1;
+//   }
+// }, 5000);
+
+// let menu = document.querySelector("#menu-icon");
+// let navbar = document.querySelector(".nav-link");
+
+// menu.onclick = () => {
+//   menu.classList.toggle("bx-x");
+//   navbar.classList.toggle("open");
+// };
